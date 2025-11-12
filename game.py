c2cead95cf788b92c94644e6bee371ab22b9d15d
DiMarzioBranor <bdimarzio8889@floridapoly.edu>
2025-11-12 11:47:49 -0500

    Implement random computer choice for game
    Fix computer choice logic and add random selection.

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
+import random as rand
 print("Welcome to Rock-Paper-Scissors!")
+computer = ""
+choice = rand.randrange(0,2)
+if choice ==1:
+  computer = "Rock"
+elif choice ==2:
+  computer == "Scissors"
+else:
+  computer == "Paper"
