7ae300b26260445a11b811383bbe5dd67d9ea4b4
DiMarzioBranor <bdimarzio8889@floridapoly.edu>
2025-11-12 11:56:52 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,9 +4,9 @@ computer = ""
 choiceC = rand.randrange(0,2)
 choice = input("rock, paper, or scissors: ")
 print(f"You chose: {choice}")
-if choice ==1:
+if choiceC ==1:
   computer = "Rock"
-elif choice ==2:
+elif choiceC==2:
   computer == "Scissors"
 else:
   computer == "Paper"
